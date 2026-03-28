c1898eef22b22c661bb9963622372fb1387c141a
Rohan Kamat <rohankamat7986@gmail.com>
2026-03-28 12:26:49 +0530

    made interactive gallery

--- script.js
+++ script.js
@@ -225,12 +225,13 @@ function horizontalScroll() {
         scale: 1,
         yoyo: true,
         repeat: 1,
-        ease: 'power1.inOut',
+        ease: 'none',
+        border: '2px solid #ff8c00',
         scrollTrigger: {
           trigger: card,
           containerAnimation: mainScroll,
           start: 'left 80%',
-          end: 'right 20%',
+          end: 'right 30%',
           scrub: true,
         }
       }
@@ -255,4 +256,130 @@ button.addEventListener("click", () => {
   document.querySelector('.cont-email').value = '';
   document.querySelector('.contact-ta').value = '';
 }
-)
+);
+
+function galleryButton() {
+
+  const galleryView = document.querySelector(".gallery-view");
+  const visitItems = document.querySelectorAll(".visit-item");
+  const closeBtn = document.querySelector(".gallery-view button");
+  const leftBtn = document.querySelector(".left-button");
+  const rightBtn = document.querySelector(".right-button");
+  const pictures = document.querySelectorAll(".picture1");
+
+  let current = 0;
+
+  gsap.set(pictures, {
+    opacity: 0
+  });
+
+
+  visitItems.forEach((item, i) => {
+    item.addEventListener("click", () => {
+      current = i;
+      galleryView.style.display = "flex";
+      showPic(current);
+    });
+  });
+
+  function showPic(index) {
+
+    gsap.set(pictures, {
+      opacity: 0,
+      display: "none"
+    });
+
+    gsap.set(pictures[index], {
+      display: "flex"
+    });
+
+    gsap.fromTo(pictures[index],
+      {
+        opacity: 0,
+        scale: 0.9
+      },
+      {
+        opacity: 1,
+        scale: 1,
+        duration: 0.4
+      }
+    );
+  }
+
+
+  rightBtn.addEventListener("click", () => {
+    gsap.to(pictures[current], {
+      opacity: 0,
+      x: -30,
+      duration: 0.2,
+      onComplete: () => {
+        gsap.set(pictures[current], {
+          display: "none",
+          x: 0
+        });
+
+        current = (current + 1) % pictures.length;
+        gsap.set(pictures[current], {
+          display: "flex"
+        });
+
+        gsap.fromTo(pictures[current],
+          {
+            x: 30,
+            opacity: 0
+          },
+          {
+            x: 0,
+            opacity: 1,
+            duration: 0.3
+          }
+        );
+      }
+    });
+  });
+
+
+  leftBtn.addEventListener("click", () => {
+    gsap.to(pictures[current], {
+      opacity: 0,
+      x: 30,
+      duration: 0.2,
+      onComplete: () => {
+        gsap.set(pictures[current], {
+          display: "none",
+          x: 0
+        });
+        current = (current - 1 + pictures.length) % pictures.length;
+        gsap.set(pictures[current], {
+          display: "flex"
+        });
+        gsap.fromTo(pictures[current],
+          {
+            x: -30,
+            opacity: 0
+          },
+          {
+            x: 0,
+            opacity: 1,
+            duration: 0.3
+          }
+        );
+      }
+    });
+  });
+
+  closeBtn.addEventListener("click", () => {
+    gsap.to(galleryView, {
+      opacity: 0,
+      duration: 0.3,
+      onComplete: () => {
+        galleryView.style.display = "none";
+        gsap.set(galleryView, {
+          opacity: 1
+        });
+      }
+    });
+  });
+}
+
+galleryButton();
